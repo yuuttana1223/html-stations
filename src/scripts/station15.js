557438a2e4bff0513d8b426a71f12b13fb228587
yuuttana1223 <64090349+yuuttana1223@users.noreply.github.com>
2023-02-20 01:21:44 +0900

    ✨ station15

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -3,14 +3,33 @@ async function getData() {
   // ３秒後にフルネームのリストを表示されるようにしましょう。
   // 最初から表示されていると、クリアになりません。
   const userList = [
-    { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
-    { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
+    {
+      id: 1,
+      first_name: "優",
+      family_name: "大木",
+      affiliation: "TechTrain",
+      is_student: false,
+    },
+    {
+      id: 2,
+      first_name: "太郎",
+      family_name: "山田",
+      affiliation: "HogeHoge大学",
+      is_student: true,
+    },
   ];
-  const result = await test()
-  return await result
+  const result = await test(userList);
+  return await result;
 }
 
-function test() {
-  return
+async function test(userList) {
+  const newUserList = userList.map((user) => {
+    return {
+      ...user,
+      full_name: `${user.family_name} ${user.first_name}`,
+    };
+  });
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(newUserList), 3000);
+  });
 }
-
